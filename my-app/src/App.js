46a71f7d5cbb94aebdcf4b416354acf6e51afcac
Kyulmamedov Buludkhan <kulmamedoff2015@yandex.ru>
2024-12-11 14:23:22 +0300

    task 2 resolve

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,69 +1,73 @@
+import styles from './app.module.css';
+import data from './data.json';
 import { useState } from 'react';
-import styles from './App.module.css';
 
 export const App = () => {
-	const [value, setValue] = useState('');
-	const [list, setList] = useState([]);
-	const [error, setError] = useState('');
+	const [steps, setSteps] = useState(data);
+	const [activeIndex, setActiveIndex] = useState(0);
 
-	const onInputButtonClick = () => {
-		let promtValue = prompt('Введите значениe');
-		if (promtValue.length < 3) {
-			setError('Введенное значение должно содержать минимум 3 символа');
-			setValue('');
-		} else {
-			setValue(promtValue);
-			setError('');
+	let isOnFirstStep = activeIndex === 0;
+	let isOnLastStep = activeIndex === steps.length - 1;
+
+	const onClickGoForward = () => {
+		if (!isOnLastStep) {
+			setActiveIndex(activeIndex + 1);
 		}
 	};
 
-	const isValueValid = value.length >= 3;
-
-	const currentDate = new Date();
-	const formatedDate = ` - ${String(currentDate.getDate())}.${String(currentDate.getMonth() + 1)}.${String(currentDate.getFullYear())} ${String(currentDate.getHours())}:${String(currentDate.getMinutes())}:${String(currentDate.getSeconds())}`;
-	const updatedList = [...list, { id: Date.now(), value, createdAt: formatedDate }];
-
-	const onAddButtonClick = () => {
-		if (value.length >= 3) {
-			setList(updatedList);
-			setValue('');
-			setError('');
+	const onClickGoBack = () => {
+		if (!isOnFirstStep) {
+			setActiveIndex(activeIndex - 1);
 		}
+	};
+
+	const onClickStartOver = () => {
+		setActiveIndex(0);
 	};
 
 	return (
-		<div className={styles.app}>
-			<h1 className={styles.pageHeading}>Ввод значения</h1>
-			<p className={styles.noMarginText}>
-				Текущее значение <code>value</code>: "
-				<output className={styles.currentValue}>{value}</output>"
-			</p>
-			{error && <div className={styles.error}>{error}</div>}
-			<div className={styles.buttonsContainer}>
-				<button onClick={onInputButtonClick} className={styles.button}>
-					Ввести новое
-				</button>
-				<button
-					onClick={onAddButtonClick}
-					className={styles.button}
-					disabled={!isValueValid}
-				>
-					Добавить в список
-				</button>
-			</div>
-			<div className={styles.listContainer}>
-				<h2 className={styles.listHeading}>Список:</h2>
-				{list.length === 0 && (
-					<p className={styles.noMarginText}>Нет добавленных элементов</p>
-				)}
-				<ul className={styles.list}>
-					{list.map((item) => (
-						<li key={item.id} className={styles.listItem}>
-							{item.value}
-							<span>{item.createdAt}</span>
-						</li>
-					))}
-				</ul>
+		<div className={styles.container}>
+			<div className={styles.card}>
+				<h1>Инструкция по готовке пельменей</h1>
+				<div className={styles.steps}>
+					<div className={styles['steps-content']}>
+						{steps[activeIndex].content}
+					</div>
+					<ul className={styles['steps-list']}>
+						{steps.map((item, index) => {
+							let stepClasses = styles['steps-item'];
+							if (index < activeIndex) stepClasses += ' ' + styles.done;
+							if (index === activeIndex) stepClasses += ' ' + styles.active;
+
+							return (
+								<li key={index} className={stepClasses}>
+									<button
+										onClick={() => setActiveIndex(index)}
+										className={styles['steps-item-button']}
+									>
+										{index + 1}
+									</button>
+									Шаг {index + 1}
+								</li>
+							);
+						})}
+					</ul>
+					<div className={styles['buttons-container']}>
+						<button
+							onClick={onClickGoBack}
+							className={styles.button}
+							disabled={isOnFirstStep}
+						>
+							Назад
+						</button>
+						<button
+							onClick={isOnLastStep ? onClickStartOver : onClickGoForward}
+							className={styles.button}
+						>
+							{isOnLastStep ? 'Начать сначала' : 'Далее'}
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
